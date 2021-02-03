c29713ebd5b1433419ac8e18b37836becf4f364e
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-02-04 01:59:43 +0300

    Initial Commit

---
+++ HomeWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork3
+//
+//  Created by ALEKSEY SUSLOV on 04.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
