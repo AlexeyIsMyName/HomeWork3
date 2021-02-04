a993709243564013c0b24259ac2dc41cbcbb953e
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-02-04 04:00:59 +0300

    made login and welcome views correctly and almost all related logic

---
+++ HomeWork3/LoginViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  HomeWork3
+//
+//  Created by ALEKSEY SUSLOV on 04.02.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private Properties
+    private let userName = "Donald"
+    private let password = "MAGA2020"
+    
+    // MARK: - IB Actions
+    @IBAction func forgotButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0: showAlarmWith(title: "Oops!",
+                              message: "Your name is \(userName)  🧐")
+        case 1: showAlarmWith(title: "Oops!",
+                              message: "Your password is \(password)  🧐")
+        default: break
+        }
+    }
+    
+    @IBAction func logInButtonPressed() {
+        if userNameTextField.text != userName || passwordTextField.text != password {
+            showAlarmWith(title: "Invalid login or password",
+                          message: "Please, enter correct login and password")
+        }
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userName
+    }
+    
+    // MARK: - Private Methods
+    private func showAlarmWith(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
